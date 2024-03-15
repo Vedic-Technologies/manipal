bbafa76af57e048968b4d7e06ea979dec916941f
abhinavbgpgit <abhinavbgp@gmail.com>
2024-03-16 00:11:17 +0530

    ok

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Login from './pages/Login';
+import Login from './pages/login/Login';
 
 function App() {
 
--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,3 +1,4 @@
+import LoginBlock from "./LoginBlock"
 
 const Login = () => {
   return (
@@ -5,7 +6,7 @@ const Login = () => {
     <div className="w-[60%] h-5/6 bg-white  rounded-2xl flex bg-login-image bg-cover">
       <div className="w-1/2  h-full  rounded-tl-2xl rounded-bl-2xl ">    
       </div>
-      <div className="w-1/2  h-full"></div>
+      <div className="w-1/2  h-full"><LoginBlock/></div>
     </div>
     </div>
   )
