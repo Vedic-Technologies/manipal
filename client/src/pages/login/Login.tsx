56ad87be118a4be7b443a28324e0390fd484c85a
ashish <ashish.2octomber@gmail.com>
2024-03-21 23:32:22 +0530

    css design completed

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -2,11 +2,11 @@ import LoginBlock from "./LoginBlock"
 
 const Login = () => {
   return (
-    <div className="bg-gradient-to-r from-blue-400 to-blue-300 h-screen flex items-center justify-center">
-    <div className="w-[60%] h-5/6 bg-white  rounded-2xl flex bg-login-image bg-cover">
+    <div className="bg-gradient-to-r from-cyan-100 to-teal-50 h-screen flex items-center justify-center">
+    <div className="w-[60%] h-5/6 bg-white justify-center items-center rounded-2xl flex bg-login-image bg-cover relative ">
       <div className="w-1/2  h-full  rounded-tl-2xl rounded-bl-2xl ">    
       </div>
-      <div className="w-1/2  h-full"><LoginBlock/></div>
+      <div className="w-full justify-center flex mr-80 h-full"><LoginBlock/></div>
     </div>
     </div>
   )
